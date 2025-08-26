4f08d59e334c4dce6f8bb86614dcb9ef3b177b97
XXRadeonXFX <prince.beats01@gmail.com>
2025-08-26 02:14:47 -0700

    new-changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,21 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # DB Instance
 resource "aws_instance" "db" {
   ami                         = var.ami_id
   instance_type               = var.instance_type_db
-  subnet_id                   = var.subnet_id
+  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]   # first subnet
   vpc_security_group_ids      = [var.sg_id]
   key_name                    = var.key_name
   associate_public_ip_address = true
@@ -17,7 +30,7 @@ resource "aws_instance" "db" {
 resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type_web
-  subnet_id                   = var.subnet_id
+  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]   # first subnet
   vpc_security_group_ids      = [var.sg_id]
   key_name                    = var.key_name
   associate_public_ip_address = true
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,6 +1,6 @@
 aws_region = "ap-south-1"
 key_name   = "prince-pair-x-2"
 ami_id     = "ami-02d26659fd82cf299"
-subnet_id  = "subnet-0fe9c16f384ed76e"     # pick one from your VPC
-sg_id      = "sg-036f8d6933b57505b"        # ID of TravelMemorySG-Prince
+subnet_id  = "subnet-1234567890abcdef" 
+sg_id      = "sg-036f8d6933b57505b" 
 project_name = "prince-travelmemory-observability"
